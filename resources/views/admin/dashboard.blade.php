f51a3344ae73b9c441081e51b4d60fc4d3014203
abdlbaqi <fuadbaqi910@gmail.com>
2025-10-30 23:08:40 +0700

    add dashboard admin

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard Admin</title>
+</head>
+<body>
+    <h1>Ini adalah Dashboard Admin</h1>
+    <p>Selamat datang, {{ Auth::user()->name }}!</p>
+
+    {{-- Tombol Logout --}}
+    <form action="{{ route('logout') }}" method="POST" style="margin-top: 20px;">
+        @csrf
+        <button type="submit">Logout</button>
+    </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Admin\AdminDashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
@@ -11,16 +12,16 @@ use App\Http\Controllers\Auth\ForgotPasswordController;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| Di sini kamu bisa mendefinisikan semua route aplikasi kamu.
+| Route ini akan dimuat oleh RouteServiceProvider dalam group "web".
 |
 */
 
-
+// 🔹 Halaman utama dan gallery (umum)
 Route::get('/', [DashboardController::class, 'index'])->name('home');
 Route::get('/gallery-hewan', [DashboardController::class, 'hewan'])->name('gallery.hewan');
 
+// 🔹 Route hanya untuk tamu (belum login)
 Route::middleware('guest')->group(function () {
     Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
     Route::post('login', [LoginController::class, 'login']);
@@ -31,4 +32,24 @@ Route::middleware('guest')->group(function () {
     Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
 });
 
-Route::post('logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
+// 🔹 Logout hanya untuk user login
+Route::post('logout', [LoginController::class, 'logout'])
+    ->name('logout')
+    ->middleware('auth');
+
+// 🔹 Route untuk dashboard ADMIN
+Route::middleware(['auth'])->group(function () {
+    Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])
+        ->name('admin.dashboard');
+});
+
+// 🔹 (Opsional) Route untuk dashboard SUPERADMIN & PEMBELI
+Route::middleware(['auth'])->group(function () {
+    Route::get('/superadmin/dashboard', function () {
+        return view('superadmin.dashboard');
+    })->name('superadmin.dashboard');
+
+    Route::get('/pembeli/dashboard', function () {
+        return view('pembeli.dashboard');
+    })->name('pembeli.dashboard');
+});
