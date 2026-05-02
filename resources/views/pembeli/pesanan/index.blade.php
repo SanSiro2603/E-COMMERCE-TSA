c7cb03c78461201da4aa44fc90bca6e52be9bd10
abdlbaqi <fuadbaqi910@gmail.com>
2026-05-02 17:44:57 +0700

    riwayat transaksi to riwayat pesanan

--- resources/views/pembeli/pesanan/index.blade.php
+++ resources/views/pembeli/pesanan/index.blade.php
@@ -48,7 +48,7 @@
     <div class="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4">
         <div>
             <h1 class="text-2xl md:text-3xl font-bold text-gray-900 dark:text-white font-be-vietnam">
-                Riwayat Transaksi Anda
+                Riwayat Pesanan
             </h1>
             <p class="text-sm text-gray-600 dark:text-zinc-400 mt-1">
                 Kelola dan pantau pesanan Anda
