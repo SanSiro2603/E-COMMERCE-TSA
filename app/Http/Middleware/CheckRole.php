4fae749d941601a0bbbacde70872e8896e759925
Indra Andrean <143471956+IndraAndrean@users.noreply.github.com>
2026-06-29 23:10:16 +0700

    perbaikan chekrole.php

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -32,10 +32,14 @@ class CheckRole
 
         $response = $next($request);
 
-        return $response->withHeaders([
-            'Cache-Control' => 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0',
-            'Pragma'        => 'no-cache',
-            'Expires'       => 'Sat, 01 Jan 2000 00:00:00 GMT',
-        ]);
+        if (method_exists($response, 'withHeaders')) {
+            return $response->withHeaders([
+                'Cache-Control' => 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0',
+                'Pragma'        => 'no-cache',
+                'Expires'       => 'Sat, 01 Jan 2000 00:00:00 GMT',
+            ]);
+        }
+
+        return $response;
     }
 }
